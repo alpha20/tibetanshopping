131fcc41c684afbc025483adcc665d09db0426f6
Ngawang Yeshi <ngawang2yeshi@gmail.com>
2017-09-25 14:41:03 +0530

    tibetan traditional dress

---
+++ shoppingbackend/src/main/java/com/drongma/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,18 @@
+package com.drongma.shoppingbackend.dao;
+
+import java.util.List;
+
+import com.drongma.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	
+	
+	Category get(int id);
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
+	
+	
+}
